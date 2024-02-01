0a43f9c807834dfc44d92e9e271134314b93aac2
ljj <119010147@link.cuhk.edu.cn>
2024-01-31 19:05:52 -0500

    complete three requirements

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,14 +3,22 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
+    private Shape shape;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    Renderer(Shape shape_todraw) {
+        this.shape = shape_todraw;
+    }
+
+    public Shape getShape() {
+        return this.shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        double area = shape.getArea();
 
         // assume implementation
 
